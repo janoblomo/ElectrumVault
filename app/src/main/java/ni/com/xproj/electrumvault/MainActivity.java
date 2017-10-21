68b068d41b3efaf7e49b0499209c6d17d1026f78
janoblomo <janoblomo@gmail.com>
2017-10-21 00:58:52 -0600

    Initial commit

---
+++ app/src/main/java/ni/com/xproj/electrumvault/MainActivity.java
@@ -0,0 +1,13 @@
+package ni.com.xproj.electrumvault;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
